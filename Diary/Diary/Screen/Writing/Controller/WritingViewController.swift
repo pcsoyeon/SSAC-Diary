161a13bd7a86ac35f96affdf7db94058f79e81c2
so yeon <alicesysy@swu.ac.kr>
2022-08-22 00:39:09 +0900

    add : SPM 파일 추가 (#1)

---
+++ Diary/Diary/Screen/Writing/Controller/WritingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WritingViewController.swift
+//  Diary
+//
+//  Created by 소연 on 2022/08/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WritingViewController: BaseViewController {
+    
+}
