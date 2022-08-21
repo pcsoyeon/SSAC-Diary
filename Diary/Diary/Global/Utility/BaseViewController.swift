2979ed4f2d599a877dd4b9d551320cccfd615379
so yeon <alicesysy@swu.ac.kr>
2022-08-22 00:33:36 +0900

    init : 폴더링 및 base 파일 추가 (#1)

---
+++ Diary/Diary/Global/Utility/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.swift
+//  Diary
+//
+//  Created by 소연 on 2022/08/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        configure()
+    }
+    
+    func configure() { }
+    
+    func showAlertMessage(title: String, button: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let ok = UIAlertAction(title: button, style: .default)
+        alert.addAction(ok)
+        present(alert, animated: true)
+    }
+}
