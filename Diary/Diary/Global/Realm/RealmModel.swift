2addd2e560c4456e06aff260998f841eb6a52cf1
so yeon <alicesysy@swu.ac.kr>
2022-08-22 11:05:39 +0900

    fix : UIScrollView로 화면구조 변경 (#7)

---
+++ Diary/Diary/Global/Realm/RealmModel.swift
@@ -0,0 +1,8 @@
+//
+//  RealmModel.swift
+//  Diary
+//
+//  Created by 소연 on 2022/08/22.
+//
+
+import Foundation
--- Diary/Diary/Screen/Writing/View/WritingView.swift
+++ Diary/Diary/Screen/Writing/View/WritingView.swift
@@ -14,6 +14,14 @@ final class WritignView: BaseView {
     
     // MARK: - UI Property
     
+    private var scrollView = UIScrollView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    private var contentView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
     var imageView = UIImageView().then {
         $0.contentMode = .scaleAspectFit
         $0.backgroundColor = .systemGray5
@@ -45,22 +53,34 @@ final class WritignView: BaseView {
     // MARK: - UI Method
     
     override func configureUI() {
-        addSubviews([imageView,
-                     titleTextField,
-                     subTitleTextField,
-                     contentTextView,
-                     searchButton])
+        self.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews([imageView,
+                                 searchButton,
+                                 titleTextField,
+                                 subTitleTextField,
+                                 contentTextView])
     }
     
     override func setConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.greaterThanOrEqualToSuperview()
+        }
+        
         imageView.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide)
-            make.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(250)
         }
         
         searchButton.snp.makeConstraints { make in
-            make.trailing.equalTo(self.safeAreaLayoutGuide).inset(35)
+            make.trailing.equalToSuperview().inset(35)
             make.bottom.equalTo(imageView.snp.bottom).inset(15)
             make.width.equalTo(55)
             make.height.equalTo(30)
@@ -68,20 +88,21 @@ final class WritignView: BaseView {
         
         titleTextField.snp.makeConstraints { make in
             make.top.equalTo(imageView.snp.bottom).offset(15)
-            make.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(44)
         }
         
         subTitleTextField.snp.makeConstraints { make in
             make.top.equalTo(titleTextField.snp.bottom).offset(15)
-            make.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(44)
         }
         
         contentTextView.snp.makeConstraints { make in
             make.top.equalTo(subTitleTextField.snp.bottom).offset(15)
-            make.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
-            make.height.equalTo(250)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(500)
+            make.bottom.equalToSuperview()
         }
     }
 }
