19899d909764715da72cdd6efaea6bb0c42cdf9f
so yeon <alicesysy@swu.ac.kr>
2022-08-28 00:27:00 +0900

    refactor : UITabBarController로 앱 플로우 변경 (#16)

---
+++ Diary/Diary/Screen/Main/Controller/TabBarController.swift
@@ -0,0 +1,54 @@
+//
+//  TabBarController.swift
+//  Diary
+//
+//  Created by 소연 on 2022/08/28.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTabBarController()
+        setTabBarAppearence()
+    }
+    
+    // MARK: - UI Method
+    
+    private func configureTabBarController() {
+        let mainViewController = UINavigationController(rootViewController: MainViewController())
+        mainViewController.tabBarItem.title = "메인"
+        mainViewController.tabBarItem.image = UIImage(systemName: "house")
+        
+        let searchViewController = UIViewController()
+        searchViewController.tabBarItem.title = "검색"
+        searchViewController.tabBarItem.image = UIImage(systemName: "star")
+        
+        let settingViewController = UIViewController()
+        settingViewController.tabBarItem.title = "설정"
+        settingViewController.tabBarItem.image = UIImage(systemName: "tray")
+        
+        setViewControllers([mainViewController, searchViewController, settingViewController], animated: true)
+    }
+    
+    private func setTabBarAppearence() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .clear
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+        tabBar.tintColor = .systemBlue
+    }
+}
+
+// MARK: - UITabBarController Delegate
+
+extension TabBarController: UITabBarControllerDelegate {
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print(item)
+    }
+}
